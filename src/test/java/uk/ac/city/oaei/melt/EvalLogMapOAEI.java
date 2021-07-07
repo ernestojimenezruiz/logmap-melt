e9f495ed999b86b25a6de0ddd724cc1d27706aca
root <root@Vava-Precision-7710>
2021-07-07 12:16:27 +0100

    Improved test class

--- src/test/java/uk/ac/city/oaei/melt/EvalLogMapOAEI.java
+++ src/test/java/uk/ac/city/oaei/melt/EvalLogMapOAEI.java
@@ -18,45 +18,62 @@ public class EvalLogMapOAEI {
     
 	private static final Logger LOGGER = LoggerFactory.getLogger(EvalLogMapOAEI.class);
 	
+	private static enum System {MatcherURL, MactherOWLAPI, SimpleJena, NODocker};
 		
     public static void main(String[] args) throws Exception {
     
     	    	
     	File dockerFile;
+    	MatcherDockerFile dockerMatcher;
+    	ExecutionResultSet ers;
     	
-        dockerFile = new File("/home/ernesto/Documents/OAEI2021/evaluation/logmap-melt-oaei-2021-web-latest.tar.gz");
-        //dockerFile = new File("/home/ernesto/Documents/OAEI2021/evaluation/simplewebmatcher-1.0-web-latest.tar.gz");
-        
-        
-        MatcherDockerFile dockerMatcher;                  
-        dockerMatcher = new MatcherDockerFile("logmap-melt-oaei-2021-web:latest", dockerFile);//
-        //dockerMatcher = new MatcherDockerFile("simplewebmatcher-1.0-web:latest", dockerFile);//
-        
-        //LOGGER.info(MatcherDockerFile.getImageNameFromFile(dockerFile));
-        dockerMatcher.logAllLinesFromContainer();
-        
-        // running the matcher on any task
-        ExecutionResultSet ers = Executor.run(TrackRepository.Anatomy.Default.getFirstTestCase(), dockerMatcher);
-        //ExecutionResultSet ers = Executor.run(TrackRepository.Conference.V1.getFirstTestCase(), dockerMatcher);
-
-        
-        //LogMap_MELT matcher = new LogMap_MELT();
-        //ExecutionResultSet ers = Executor.run(TrackRepository.Conference.V1.getFirstTestCase(), matcher);
-        //ExecutionResultSet ers = Executor.run(TrackRepository.Anatomy.Default.getFirstTestCase(), matcher);
-        
-        
+    	
+    	System system;
+    	system = System.MactherOWLAPI;
+    	//system = System.MatcherURL;
+    	//system = System.SimpleJena;
+    	
+    	if (system == System.NODocker) { //Local MELT system or built-in MELT matchers
+    		
+    		LogMap_MELT matcher = new LogMap_MELT();
+            //ers = Executor.run(TrackRepository.Conference.V1.getFirstTestCase(), matcher);
+            ers = Executor.run(TrackRepository.Anatomy.Default.getTestCases(), matcher);
+            
+    	}
+    	else {
+	    	if (system == System.MactherOWLAPI) {
+		        dockerFile = new File("/home/ernesto/Documents/OAEI2021/evaluation/logmap-melt-owlapi-oaei-2021-web-latest.tar.gz");
+		        dockerMatcher = new MatcherDockerFile("logmap-melt-owlapi-oaei-2021-web:latest", dockerFile);//
+	    	}
+	    	else if (system == System.MatcherURL) {
+		        dockerFile = new File("/home/ernesto/Documents/OAEI2021/evaluation/logmap-melt-oaei-2021-web-latest.tar.gz");
+		        dockerMatcher = new MatcherDockerFile("logmap-melt-oaei-2021-web:latest", dockerFile);//
+	    	}
+	    	else {// if (system == System.SimpleJena) {
+	    		dockerFile = new File("/home/ernesto/Documents/OAEI2021/evaluation/simplewebmatcher-1.0-web-latest.tar.gz");
+	    		dockerMatcher = new MatcherDockerFile("simplewebmatcher-1.0-web:latest", dockerFile);//
+	    	}
+	    	
+	    	//LOGGER.info(MatcherDockerFile.getImageNameFromFile(dockerFile));
+	        dockerMatcher.logAllLinesFromContainer();
+	        
+	        // running the matcher on any task
+	        //ers = Executor.run(TrackRepository.Anatomy.Default.getFirstTestCase(), dockerMatcher);
+	        ers = Executor.run(TrackRepository.Conference.V1.getTestCases(), dockerMatcher);
+	        
+	        
+	        //TODO
+	        // we should close the docker matcher so that docker cab shut down the container
+	        //Leave it open for debugging purposes to access the logs: "docker container logs {id_container}"
+	        //dockerMatcher.close();
+	    
+    	}
+       
         
         // evaluating our system
         EvaluatorCSV evaluatorCSV = new EvaluatorCSV(ers);
         EvaluatorBasic evaluatorBasic = new EvaluatorBasic(ers);        
         EvaluatorCopyResults evalCopy = new EvaluatorCopyResults(ers);
-
-        
-        //TODO
-        // we should close the docker matcher so that docker cab shut down the container
-        //Leave it open for debugging purposes to access the logs: "docker container logs {id_container}"
-        //dockerMatcher.close();  
-        
 
         // writing evaluation results to disk
         evaluatorCSV.writeToDirectory("/home/ernesto/Documents/OAEI2021/evaluation/results/");
